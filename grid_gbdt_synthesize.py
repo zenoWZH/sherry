0582347f616d42b09304b68c2b16876796080e7e
zenoWZH <wangzhouhao94@gmail.com>
2022-11-27 20:37:00 -0800

    modify parameters

---
+++ grid_gbdt_synthesize.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import warnings
+import json
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.model_selection import cross_val_score, ShuffleSplit
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
+
+df = pd.read_csv("./data_synthesize/out/correlated_attribute_mode/sythetic_data.csv")
+data =df.values
+
+#scaler = MinMaxScaler()
+scaler = StandardScaler()
+result_feature = scaler.fit_transform(data[:,:18])
+result_label = data[:,18]
+result = np.append(result_feature, result_label.reshape(len(result_label),1), axis = 1)
+df_newdata = pd.DataFrame(result, columns= df.columns)
+
+
+Y = df["Outcome"].values
+X = df[['Gender', 'Age', 'Height', 'Weight', 'BMI', 'Hypertension',
+       'SBP', 'DBP', 'PR', 'Drink', 'Smoke', 'FPG', 'AST', 'ALT', 'BUN', 'Scr',
+       'TG', 'TC']].values
+names = ['Gender', 'Age', 'Height', 'Weight', 'BMI', 'Hypertension',
+       'SBP', 'DBP', 'PR', 'Drink', 'Smoke', 'FPG', 'AST', 'ALT', 'BUN', 'Scr',
+       'TG', 'TC']
+
+
+score = 'f1'
+
+param_dist = {'max_depth': [3,4,5,6,7,8,9,10],
+              'max_features': ['auto', 'sqrt'],
+              'min_samples_leaf': [1, 2, 4],
+              'min_samples_split': [2, 3, 4, 5, 7, 10],
+              'n_estimators': range(20,80)}
+
+clf = GridSearchCV(RandomForestClassifier(), param_dist, cv=ShuffleSplit(5, test_size = .2, train_size = .8), scoring='%s_macro' % score, n_jobs= 6)#, verbose=10)
+
+clf.fit(X, Y)
+
+print(clf.best_params_)
+#with open("bestparams_xgb.json", "wb") as xgbjs:
+#       json.dump(clf.best_params_, xgbjs)
